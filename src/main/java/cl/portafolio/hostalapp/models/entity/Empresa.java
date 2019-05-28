0586f53ea6265ba9f49b81362b31b721f18a62c7
ro <rodolfo.quezada.s@gmail.com>
2019-05-27 20:38:17 -0400

    mas cambios nuevo contructor en empresa

--- src/main/java/cl/portafolio/hostalapp/models/entity/Empresa.java
+++ src/main/java/cl/portafolio/hostalapp/models/entity/Empresa.java
@@ -2,11 +2,13 @@ package cl.portafolio.hostalapp.models.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
@@ -43,5 +45,18 @@ public class Empresa implements Serializable {
     @JsonIgnore
     @OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Huesped> huespedList; */
+
+
+    public Empresa(String json) throws IOException {
+        Empresa e = new ObjectMapper().readValue(json, Empresa.class);
+        this.id = e.getId();
+        this.rut = e.getRut();
+        this.razonSocial = e.getRazonSocial();
+        this.nombreEmpresa = e.getNombreEmpresa();
+        this.direccionFacturacion = e.getDireccionFacturacion();
+        this.telefono = e.getTelefono();
+        this.representanteLegal = e.getRepresentanteLegal();
+    }
+
 
 }
---
+++ src/test/java/cl/portafolio/hostalapp/models/services/impl/RepresentanteLegalServiceImplTest.java
@@ -0,0 +1,7 @@
+package cl.portafolio.hostalapp.models.services.impl;
+
+import static org.junit.Assert.*;
+
+public class RepresentanteLegalServiceImplTest {
+
+}
